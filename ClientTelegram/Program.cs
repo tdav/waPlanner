fef90593968a8090e49bec83dfba37fe4ab79628
sardorahmedovt557 <sardorahmedovt557@gmail.com>
2022-05-01 17:49:32 +0500

    2222

--- ClientTelegram/Program.cs
+++ ClientTelegram/Program.cs
@@ -14,13 +14,13 @@ namespace ClientTelegram
 
         public async static Task TClient()
         {
-            int apiId = 0;
-            string apiHash = "hash";
 
             using var client = new Client();
-            var me = await client.LoginUserIfNeeded();
+            await client.LoginUserIfNeeded();
+
             Contacts_ResolvedPeer my_bot = await client.Contacts_ResolveUsername("clinic_test_uzbot");
-            var new_user = await client.Contacts_ImportContacts(new[] { new InputPhoneContact { phone = "+998900090250" } });
+            var new_user = await client.Contacts_ImportContacts(new[] { new InputPhoneContact { phone = "+998900090250", 
+                first_name = "Organization", last_name = "User_Name"} });
 
             var create_group = await client.Channels_CreateChannel("TEST", "TEST", megagroup: true);
             var group = create_group.Chats.GetEnumerator();
@@ -28,10 +28,16 @@ namespace ClientTelegram
             long group_id = group.Current.Key;
 
             await client.AddChatUser(create_group.Chats[group_id], my_bot.User);
-            var mcf = await client.GetFullChat(create_group.Chats[group_id]);
-            var invite = (ChatInviteExported)mcf.full_chat.ExportedInvite;
-            await client.SendMessageAsync(new_user.users[new_user.imported[0].user_id], "Join to this group " + invite.link);
+            var get_chat = await client.GetFullChat(create_group.Chats[group_id]);
+            var invite = (ChatInviteExported)get_chat.full_chat.ExportedInvite;
+            await client.SendMessageAsync(new_user.users[new_user.imported[0].user_id], 
+                "—сылка дл€ вашей группы организации " + invite.link);
         }
+
+        private static string Config(string what)
+        {
+            
+        } 
     }
 }
 
